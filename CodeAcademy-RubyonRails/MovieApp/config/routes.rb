ec09e5937747d15aa61987892957aed0c97046a3
theresabolaney <49379116+theresabolaney@users.noreply.github.com>
2025-07-09 20:54:25 -0500

    Add files via upload

---
+++ CodeAcademy-RubyonRails/MovieApp/config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get '/movies', to: 'movies#index'
+  get '/movies/:id', to: 'movies#show', as: :movie
+  get '/actors', to: 'actors#index'
+  get '/actors/:id', to: 'actors#show', as: :actor
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
